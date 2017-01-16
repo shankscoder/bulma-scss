7e8240d4dd1fbfcd7c0266c7089212d33942b435
Ashwin Shankar <shankscoder@users.noreply.github.com>
2017-01-17 01:12:17 +0530

    Changed clone target to my repo for debugging.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=${1:-master} # e.g. 0.0.18
 rm -rf $DIR/../bulma
 cd $DIR/..
-git clone https://github.com/jgthms/bulma.git
+git clone https://github.com/shankscoder/bulma.git
 cd $DIR/../bulma
 git fetch origin --tags
 git checkout $VERSION
